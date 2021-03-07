eada2852a1317b391295f18f63eea4ac72f3ceac
henok Gebremedhn <henoklwi@gmail.com>
2021-03-07 01:31:39 -0800

    modified my router.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const products = require('./data/products.js');
 const connectDB = require('./config/db.js');
+const productHandler = require('./Routers/productRouter.js');
 require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
 
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('app send');
-});
-
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((index) => index._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productHandler);
 
 app.listen(PORT, console.log(`app is listening at ${PORT}`));
